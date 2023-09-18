c4c9df9192f3cfec4058fc9e9ec7664e88eb2fb8
MarcelloMello90 <marcellomello90@gmail.com>
2023-09-18 03:25:57 +0200

    finish project

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -4,9 +4,27 @@ import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from '@ffmpeg/util'
+import { api } from "@/lib/axios"
 
-export function VideoInputForm(){
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!',
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id:string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps){
   const [ videoFile, setVideoFile ] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
+
   const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>){
@@ -21,10 +39,47 @@ export function VideoInputForm(){
     setVideoFile(selectedFile)
   }
 
-  function convertVideoToAudio(){}
+  async function convertVideoToAudio(video: File){
+    console.log('Convert started')
+
+    const ffmpeg = await getFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    // ffmpeg.on('log', log => {
+    //   console.log(log)
+    // })
+
+    ffmpeg.on('progress', progress => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+
+    const audioFileBlob = new Blob([data], {type: 'audio/mpeg'})
+    const audioFile = new File([audioFileBlob], 'audio.mp3', {
+      type: 'audio/mpeg'
+    })
+
+    console.log('Convert finished')
+
+    return audioFile
+  }
 
 
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>){
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>){
     event.preventDefault()
 
     const prompt = promptInputRef.current?.value
@@ -32,7 +87,34 @@ export function VideoInputForm(){
     if(!videoFile){
       return
     }
+    setStatus('converting')
+
+    // converter video em audio
+    const audioFile = await convertVideoToAudio(videoFile)
+
+    console.log(audioFile, prompt)
+
+    // conectando front end e back end
+    const data = new FormData()
+
+    data.append('file', audioFile)
+
+    setStatus('uploading')
+
+    const response = await api.post('/videos', data)
     
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    })
+
+    setStatus('success')
+
+    props.onVideoUploaded(videoId)
   }
 
   const previewURL = useMemo(() => {
@@ -67,14 +149,24 @@ export function VideoInputForm(){
         <Label htmlFor="transcription_prompt"> Prompt de transcrição</Label>
         <Textarea 
           ref={promptInputRef}
+          disabled={status !== 'waiting'}
           id='transcription_prompt'
           className='h-20 leading-relaxed resize-none'
           placeholder='Inclua palavra-chave mencionadas no vídeo separadas por vírgula (,)'
         />
       </div>
-      <Button type='submit' className='w-full'>
-        Carregar vídeo
-        <Upload className="w-4 h-4 ml-2"/>
+      <Button 
+        data-success={status === 'success'}
+        disabled={status !== 'waiting'} 
+        type='submit' 
+        className='w-full data-[success=true]:bg-emerald-400'
+      >
+       {status === 'waiting' ? (
+         <>
+          Carregar vídeo
+          <Upload className="w-4 h-4 ml-2"/>
+         </>
+       ) : statusMessages[status]}
       </Button>
     </form>
   )
